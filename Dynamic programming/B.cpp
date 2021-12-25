7ba62ce3b4b11cf19dbec004f3ddb2717f325376
Nikita Panyuhin <n.panuhin@mail.ru>
2021-12-25 14:39:16 +0300

    Added README for "Stacks, queues, DSU"

--- Dynamic programming/B.cpp
+++ Dynamic programming/B.cpp
@@ -266,7 +266,7 @@ signed main() {
     }
 
     d[0][0] = field[0][0];
-    
+
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             if (i == 0 && j == 0) continue;
